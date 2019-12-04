7ccc3ce38304907af47cbb3e8f92fd1d7d128f99
Matej Mrnjec <matej.mrnjec@hotmail.com>
2019-12-04 22:03:44 +0100

    Added Comments

--- football-app-pwa-workbox/app/sw.js
+++ football-app-pwa-workbox/app/sw.js
@@ -3,30 +3,33 @@ importScripts('./scripts/libs/workbox-sw-3.5.0.js');
 if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`);
 
+    // Precache all static Files and register route
     workbox.precaching.precacheAndRoute([], {
         // Ignore all URL parameters.
         ignoreUrlParametersMatching: [/.*/]
     });
 
+    // Register Competitions Route with CacheFirst Strategy
     workbox.routing.registerRoute(
         /\b\/competitions(?!\/)/,
         workbox.strategies.cacheFirst({
             cacheName: 'competition-cache',
             plugins: [
                 new workbox.expiration.Plugin({
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                    maxAgeSeconds: 7 * 24 * 60 * 60, // Expires in 7 Days
                 })
             ]
         })
     );
 
+    // Register Competitions Route with staleWhileRevalidate Strategy
     workbox.routing.registerRoute(
         /\/competitions\/[0-9]+\/matches/,
         workbox.strategies.staleWhileRevalidate({
             cacheName: 'match-cache',
             plugins: [
                 new workbox.expiration.Plugin({
-                    maxEntries: 20
+                    maxEntries: 20  // Max 20 Responses should be saved
                 })
             ]
         })
